04185b2fc1437de5d33e030df59ab48a6b8f3cf9
Eolynas <eddy.hubert@yahoo.fr>
2018-12-06 16:33:35 +0100

    first commit

---
+++ app/App.php
@@ -0,0 +1,25 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of App
+ *
+ * @author Eddy
+ */
+class App {
+    public $title = "Mon super site";
+    private $db_instance;
+    private static $_instance;
+
+    public static function getInstance(){
+        if(is_null(self::$_instance)){
+            self::$_instance = new App();
+        }
+        return self::$_instance;
+    }
+}
